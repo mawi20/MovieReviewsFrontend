60cc7616d596908c5df9fb13cba8eb06717240de
mawi <dagmawi20@gmail.com>
2017-11-30 13:51:14 -0500

    Added cookies

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -12,3 +12,23 @@ $(() => {
 
 // use require without a reference to ensure a file is bundled
 require('./example')
+
+$('#sign-up').submit(function (e) {
+  const signUpEmail = $('#sign-up-email').val()
+  const signUpPassword = $('#sign-up-password').val()
+  const signUpPasswordConfirmation = $('#sign-up-password-confirmation').val()
+  const data = {'credentials': {'email': signUpEmail, 'password': signUpPassword, 'password_confirmation': signUpPasswordConfirmation}}
+  const url = 'http://tic-tac-toe.wdibos.com/sign-up'
+  $.ajax({
+    type: 'POST',
+    url: url,
+    data: data,
+    dataType: 'JSON',
+    success: function (data) {
+      Cookies.set('userdata', {'id': data.user.id, 'email': data.user.email})
+      $('#game').show()
+      // console.log(data)
+    }
+  })
+  e.preventDefault()
+})
